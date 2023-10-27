7d25e01d3aa290e234d80834dc44ecbaf0d6b859
leelopesz <99299548+leelopesz@users.noreply.github.com>
2023-10-26 22:55:39 -0300

    ultimos exercicio
    - não consegui entender muito o exercicio 5, realizei conforme consegui

--- 4bim/exerciciosDeFixacao20231026.sql
+++ 4bim/exerciciosDeFixacao20231026.sql
@@ -18,3 +18,26 @@ after update on Clientes
 for each row
 insert into Auditoria(mensagem, data_hora)
 values ('O antigo tem o nome:',old.nome,'. E o novo cliente se chama:', new.nome);
+
+--Exercicio 4
+delimiter //
+create trigger update_cliente
+before update on Clientes
+for each row
+if new.nome = null or new.nome='' then
+insert into Auditoria(mensagem)
+values ('Há uma tentativa de atualização de cliente para nulo ou uma string vazia!');
+end if;
+//
+
+--Exercicio 5
+//
+create trigger insert_pedidos_estoque
+after insert on Pedidos
+for each row
+update Produtos set estoque = estoque - new.quantidade where produto_id;
+if estoque<5 then
+insert into Auditoria(mensagem)
+values ('O estoque está abaixo de 5');
+end if;
+//
