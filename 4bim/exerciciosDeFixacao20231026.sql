7ba8f0679765fdba322fa90583a2dc354d45c6bc
leelopesz <99299548+leelopesz@users.noreply.github.com>
2023-10-26 22:00:19 -0300

    exercicios feitos em aula

--- 4bim/exerciciosDeFixacao20231026.sql
+++ 4bim/exerciciosDeFixacao20231026.sql
@@ -1,1 +1,20 @@
+--Exercicio 1
+create trigger after_cliente_insert
+after insert on Clientes
+for each row
+insert into Auditoria(mensagem, data_hora)
+values('Está sendo inserido um novo cliente');
 
+--Exercicio 2
+create trigger before_cliente_delete
+before delete on Clientes
+for each row
+insert into Auditoria(mensagem, data_hora)
+values('Há uma tentativa de exclusão de um cliente');
+
+--Exercicio 3
+create trigger after_cliente_update
+after update on Clientes
+for each row
+insert into Auditoria(mensagem, data_hora)
+values ('O antigo tem o nome:',old.nome,'. E o novo cliente se chama:', new.nome);
